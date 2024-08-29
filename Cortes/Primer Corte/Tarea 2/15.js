98d30835559be946ddbbff08a392aa3255db3809
JOSEDAV18 <133983998+JOSEDAV18@users.noreply.github.com>
2024-08-29 15:38:04 -0500

    Cargar

---
+++ Cortes/Primer Corte/Tarea 2/15.js
@@ -0,0 +1,19 @@
+// Obtener edad
+
+//if(edad < 13) {
+//	console.log("Es niño");
+//}if(edad < 18 ){
+// 	console.log("Es adolecente");
+//} else{
+// 	console.log("Es adolecente");
+//}
+
+
+
+if(edad < 13) {
+    console.log("Es niño");
+} else if(edad < 18) {
+    console.log("Es adolescente");
+} else {
+    console.log("Es adulto");
+}
